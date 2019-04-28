54455ee1ed8e968705fe0a0b54927ee0e33b314f
Mathew Cormier <mathew.corm@gmail.com>
2019-04-28 16:54:46 -0400

    Preload dependencies and add v8-compile-cache

--- compile-process.js
+++ compile-process.js
@@ -1,55 +1,58 @@
-//require('v8-compile-cache')
+require('v8-compile-cache')
+// TODO: Fix v8-compile-cache used with child_process.fork
+require('./setup-debug')
 const debug = require('debug')('compile-process')
-debug('loading process')
 const CompilerSupplier = require("./compilerSupplier")
 
-//const cluster = require('cluster')
+preLoadDependencies()
 
-const DEFAULT_OPTIONS = { 
-  settings: { 
+const DEFAULT_OPTIONS = {
+  settings: {
     optimizer: { enabled: true, runs: 200 },
-    evmVersion: undefined 
-  } 
-}
-
-
-let solc
-
-async function getSolc() {
-  if (!solc) {
-    debug('Loading solc')
-    const supplier = new CompilerSupplier(DEFAULT_OPTIONS)
-    solc = await supplier.load()
-    debug('solc loaded')
+    evmVersion: undefined
   }
-  return solc
 }
 
-async function compile(input) {    
-    const solc = await getSolc()
 
-    debug('compiling')
-    //console.log('input: ', input)
-    const result = solc.compile(JSON.stringify(input))
-    const parsedResult = JSON.parse(result)
-    debug('done')
-    //console.log('output: ', JSON.parse(result))
-    return parsedResult
+function preLoadDependencies() {
+  // TODO: Remove unused dependencies
+  debug('preloading dependencies')
+  require("os")
+  require('solc')
+  require("semver")
+  require('path')
+  require('process')
+  debug('dependencies loaded')
+}
 
- }
- 
- // receive message from master process
- process.on('message', async (message) => {
-   try {
+
+async function compile(input) {
+  debug('Loading solc')
+  const supplier = new CompilerSupplier(DEFAULT_OPTIONS)
+  const solc = await supplier.load()
+  debug('solc loaded')
+
+  debug('compiling')
+  //console.log('input: ', input)
+  const result = solc.compile(JSON.stringify(input))
+  const parsedResult = JSON.parse(result)
+  debug('done')
+  //console.log('output: ', JSON.parse(result))
+  return parsedResult
+
+}
+
+// receive message from master process
+process.on('message', async (message) => {
+  try {
     debug(`starting compilation ${message.p}`)
 
-    const result = await compile(message.input);
-    
-    // send response to master process
-    process.send({ result });
-   } catch(err) {
-     debug('Error: %o', err)
-   }
- })
+    const result = await compile(message.input)
 
- getSolc()
+    // send response to master process
+    process.send({ result })
+  } catch (err) {
+    debug('Error: %o', err)
+  }
+})
+
