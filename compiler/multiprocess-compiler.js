5b4371ff67e86a404be23ad4a9f3e7a5ab29f8a1
Mathew Cormier <mathew.corm@gmail.com>
2019-05-07 16:49:13 -0400

    clean up multiprocess-compiler

--- compiler/multiprocess-compiler.js
+++ compiler/multiprocess-compiler.js
@@ -10,36 +10,7 @@ const { dispatchWork } = require('./dispatch-work')
 module.exports = function(solcStandardInput) {
 
     return new Promise((res, rej) => {
-        debug('start')
                 
-        const p1 = ['openzeppelin-solidity/contracts/math/SafeMath.sol',
-        'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol',
-        '/home/mathew/workspace/eblocks/eblocks/contracts/ERC20.sol',
-        '/home/mathew/workspace/eblocks/eblocks/contracts/Test.sol']
-        const p2 = ['/home/mathew/workspace/eblocks/eblocks/contracts/Migrations.sol',
-        '/home/mathew/workspace/eblocks/eblocks/contracts/Test2.sol',
-        '/home/mathew/workspace/eblocks/eblocks/contracts/Test3.sol',
-        '/home/mathew/workspace/eblocks/eblocks/contracts/Test4.sol',]
-        
-    
-        const input1 = { 
-            input: {
-                language: solcStandardInput.language,
-                settings: solcStandardInput.settings,
-                sources: p1.reduce((acc, v) => ({ ...acc, [v]: solcStandardInput.sources[v] }), {}),
-            },
-            p: 1
-        }
-        const input2 = { 
-            input: {
-                language: solcStandardInput.language,
-                settings: solcStandardInput.settings,
-                sources: p2.reduce((acc, v) => ({ ...acc, [v]: solcStandardInput.sources[v] }), {}),
-            },
-            p: 2
-        }
-
-
         debug('Generating dependency tree')
 
         const dependencyTree = new DependencyTree()
@@ -51,26 +22,10 @@ module.exports = function(solcStandardInput) {
             //debug(`miss dep: %o`, dependencyTree._filesWithMissingDependencies.map(f => f.path))    
         }
         
-        /*
-        for (const file of dependencyTree.getLeafs()) {
-            //const file = dependencyTree._files[fileName]
-            debug(`${file.path} deps: %o`, file.getAllDependencies().map(a => a.path))
-        }*/
-        /*
-        for (const filename in dependencyTree._files) {
-            const file = dependencyTree._files[filename]
-            debug(`${file.path} deps: %o`, file.dependencies.map(a => a.path))
-        }*/
 
-
-        /*
-        dependencyTree.getLeafs().forEach((node, index) => {
-            workers[index % cpus.length].addSource(node)
-        })*/
 
         debug('creating work batches')
-        //const batches = dispatchWork(dependencyTree, cpus.length)
-        const batches = dispatchWork(dependencyTree, 1)
+        const batches = dispatchWork(dependencyTree, cpus.length)
 
         debug('dispatching batches to workers')
         for (const [i] of batches.entries()) {
